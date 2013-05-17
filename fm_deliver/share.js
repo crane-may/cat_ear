e6a22b2bb8cb72723941a7786ee9d53bd634847e
crane <crane.may@gmail.com>
2013-05-17 15:36:07 +0800

    push deliver

--- fm_deliver/share.js
+++ fm_deliver/share.js
@@ -1,12 +1,23 @@
 var lk = document.getElementById("fm-download-link");
 var tar = document.createElement("a");
 tar.innerHTML = "推送";
+tar.setAttribute("href","javascript:;")
 var sep = document.createElement("span");
 sep.innerHTML = "|";
 
 document.getElementById("fm-download").insertBefore(tar,document.getElementById("download-frame"));
 document.getElementById("fm-download").insertBefore(sep,document.getElementById("download-frame"));
 
-setInterval(function() {
-  tar.setAttribute("href",lk.getAttribute("href"));
-},1000);
+tar.addEventListener("click",function() {
+  var url = "http://douban.fm"+lk.getAttribute("href");
+  
+  try{
+    var xml = new XMLHttpRequest();
+    xml.open("POST","http://radio.miaocc.com/devices/update_attr/did_b827ebaba73a",true);
+    xml.send('{"deliver":"'+url+'"}');
+    
+    alert("推送成功\n"+url);
+  }catch(e){
+    console.log(e);
+  }
+});
